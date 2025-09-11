a32abe8af0edbfe582b49fb4a7048e0db65547c2
gyunam-bark <gyunam.bark@gmail.com>
2025-09-11 21:09:31 +0900

    drizzle 의존성 이동

--- gateway/entrypoint.sh
+++ gateway/entrypoint.sh
@@ -1,5 +1,12 @@
 #!/bin/sh
 set -e
 
+echo "Running migrations..."
+npm run drizzle:generate
+npm run drizzle:migrate
+
+echo "Running seed..."
+npm run seed
+
 echo "Starting Gateway..."
 exec npm run start
